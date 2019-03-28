5b3922b4910704042ffbaf6ae50a1a9db7624c5b
ConTroll <sengeniovezet@gmail.com>
2019-03-28 20:51:35 +0100

    debit page basic

--- 01-breakdown-webapp/src/Containers/Debitpage/Debitpage.js
+++ 01-breakdown-webapp/src/Containers/Debitpage/Debitpage.js
@@ -1,10 +1,82 @@
 import React, { Component } from 'react';
+import './debits.scss';
 
 class Debitpage extends Component {
 
   render() {
     return (
-      <h1>This is the Debitpage</h1>
+      <div class="row">
+        <div class="col-md-9 d-flex justify-content-start" id="leftSide">
+          <div class="card" id="addDebit">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+            </div>
+        </div>
+      <div class="col-md-3 d-flex-row justify-content-start" id="rightSide">
+      <div class="card" id="right">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+      </div>
+      <div class="card" id="right">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+      </div>
+      <div class="card" id="right">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+      </div>
+      <div class="card" id="right">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+      </div>
+      <div class="card" id="right">
+            <div class="card-header">
+            Debits
+            </div>
+            <div class="card-body">
+              <blockquote class="blockquote mb-0">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+                <footer class="blockquote-footer">Someone famous in <cite title="Source Title">Source Title</cite></footer>
+              </blockquote>
+              </div>
+      </div>
+      </div>
+    </div>
     )
   }
 }
