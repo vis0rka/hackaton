043af91db231c4c999fd9225d019d0f58b2da1ce
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-29 00:33:18 +0100

    feat(FormBuilder.js): add file

---
+++ 01-breakdown-webapp/src/Components/FormBuilder.js
@@ -0,0 +1,124 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import _ from "lodash";
+// import DatePicker from "react-datepicker";
+class FormBuilder extends Component {
+  state = { values: {} };
+
+  getFields = (field, value) => {
+    const { type, name, placeholder, items } = field;
+    switch (type) {
+      // case "date":{
+      //   return (
+      //     <DatePicker
+      //       dateFormat="yyyy/MM/dd"
+      //       selected={new Date()}
+      //       onChange={this.handleChange} />
+      //   )
+      // }
+      case "text":
+      case "number": {
+        return (
+          <div className="form-group" key={name}>
+            <label htmlFor={name.toLowerCase()} style={{ color: "white" }}>
+              {name}
+            </label>
+            <input
+              onChange={event => {
+                this.handleChange(event.target.value, field.key || field.name);
+              }}
+              value={value}
+              type={type}
+              className="form-control"
+              id={name.toLowerCase()}
+              placeholder={placeholder}
+              required
+            />
+          </div>
+        );
+      }
+
+      case "select": {
+        return (
+          <React.Fragment key={name}>
+            <label htmlFor="amount" style={{ color: "white" }}>
+              {name}
+            </label>
+            <div className="input-group mb-3">
+              <select
+                onChange={event => {
+                  this.handleChange(
+                    event.target.value,
+                    field.key || field.name
+                  );
+                }}
+                value={value}
+                className="custom-select"
+                id={name.toLowerCase()}
+              >
+                {_.map(items, item => (
+                  <option
+                    value={item.value}
+                    key={`key-${item.value}`}
+                    // style={{ textTransform: "capitalize" }}
+                  >
+                    {item.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </React.Fragment>
+        );
+      }
+      default:
+        return null;
+    }
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("Form, handleSubmit: ", this.state.values);
+    this.props.handleSubmit(this.state.values);
+  };
+
+  handleChange = (value, key) => {
+    this.setState({
+      values: {
+        ...this.state.values,
+        [key]: value
+      }
+    });
+  };
+
+  render = () => {
+    return (
+      <form
+        style={{
+          background: "rgba(0,0,0,0.3)",
+          padding: "40px",
+          width: "600px"
+        }}
+        onSubmit={this.handleSubmit}
+      >
+        {_.map(this.props.fields, field => this.getFields(field))}
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    );
+  };
+}
+
+const mapStateToProps = store => ({
+  // connect to store?
+});
+
+const mapDispatchToProps = {
+  // dispatch actions
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(FormBuilder);
