19002ba6d23b1c9a03f3c69ebe64d0f812262bfc
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-28 15:46:08 +0100

    feat(income): GET endpoint

--- 00-breakdown-server/controller/income.controller.js
+++ 00-breakdown-server/controller/income.controller.js
@@ -45,8 +45,24 @@ const deleteIncome = (req, res) => {
   }
 };
 
+const getAllIncome = (req, res) => {
+  const { userId } = req.body;
+  if (!userId) {
+    res.status(400).json({
+      status: "error",
+      message: "Missing data!"
+    });
+  } else {
+    incomseServie
+      .getAllIncome(userId)
+      .then(data => res.status(200).json(data))
+      .catch(error => res.status(500).json(error));
+  }
+};
+
 module.exports = {
   postIncome,
   updateIncome,
-  deleteIncome
+  deleteIncome,
+  getAllIncome
 };
