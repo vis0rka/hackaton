eddbc2b8f5391f0d3cc335024a8aa85c9454e8ce
Dávid <davidracz90@gmail.com>
2019-03-28 12:52:25 +0100

    postExpenseController

--- 00-breakdown-server/controller/expense.controller.js
+++ 00-breakdown-server/controller/expense.controller.js
@@ -1,10 +1,35 @@
 const expenseService = require('../services/expenses.service');
 
-const postExpense = ( req, res) => {
+const postExpenseController = ( req, res) => {
   const { userId, amount, category } = req.body;
   if( category === 'accomodation' || category === 'car' || category === 'family' || category === 'clothes' || category === 'food' ||
     category === 'recreation' || category === 'healt' || category === 'pets' || category === 'household' || category === 'technologies' ||
     category === 'travel' || category  === 'others' && amount > 0 ) {
-
+      expenseService.postExpense(userId, amount, category)
+        .then((user) => {
+          res.status(200).json({
+            message: 'new expense added'
+          })
+        })
+        .catch(error => res.status(500).json(error));
+    } else if(!category) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Missing parameter(s): category!',
+      });
+    } else if(amount < 0) {
+      res.status(400).json({
+        status: 'error',
+        message: 'Expense must be greater than 0!',
+      });
+    } else {
+      res.status(400).json({
+        status: 'error',
+        message: 'Invalid category!'
+      });
     }
+};
+
+module.exports = {
+  postExpenseController,
 }
