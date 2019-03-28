25e19ed768194968d51438b74c36107214a175f3
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-28 15:01:52 +0100

    feat(income): add endpoint, controller, services

---
+++ 00-breakdown-server/controller/income.controller.js
@@ -0,0 +1,52 @@
+const incomseServie = require("../services/income.services");
+
+const postIncome = (req, res) => {
+  const { userId, amount } = req.body;
+  if (!userId || !amount) {
+    res.status(400).json({
+      status: "error",
+      message: "Missing data!"
+    });
+  } else {
+    incomseServie
+      .addNewIncome(userId, amount)
+      .then(data => res.status(200).json(data))
+      .catch(error => res.status(500).json(error));
+  }
+};
+
+const updateIncome = (req, res) => {
+  const { incomeId, amount } = req.body;
+  if (!incomeId || !amount) {
+    res.status(400).json({
+      status: "error",
+      message: "Missing data!"
+    });
+  } else {
+    incomseServie
+      .updateIncome(incomeId, amount)
+      .then(data => res.status(200).json(data))
+      .catch(error => res.status(500).json(error));
+  }
+};
+
+const deleteIncome = (req, res) => {
+  const { userId, incomeId } = req.body;
+  if (!userId || !incomeId) {
+    res.status(400).json({
+      status: "error",
+      message: "Missing data!"
+    });
+  } else {
+    incomseServie
+      .deleteIncome(userId, incomeId)
+      .then(data => res.status(200).json(data))
+      .catch(error => res.status(500).json(error));
+  }
+};
+
+module.exports = {
+  postIncome,
+  updateIncome,
+  deleteIncome
+};
--- 00-breakdown-server/routes/index.js
+++ 00-breakdown-server/routes/index.js
@@ -1,10 +1,19 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const userController = require('../controller/user.contoller');
+const userController = require("../controller/user.contoller");
+const incomeComtroller = require("../controller/income.controller");
 
-router.post('/register', userController.registerUser);
+router.post("/register", userController.registerUser);
 
-router.post('/login', userController.userLogin);
+router.post("/login", userController.userLogin);
+
+// income
+
+router.post("/income", incomeComtroller.postIncome);
+
+router.put("/income", incomeComtroller.updateIncome);
+
+router.delete("/income", incomeComtroller.deleteIncome);
 
 module.exports = router;
