e3128f9d0cd61aaa711c9b6e6224f91ddb599b33
Viktor <barta.viktor.ufs@gmail.com>
2019-03-29 00:22:02 +0100

    feat(breakdown) homepage styles

---
+++ 01-breakdown-webapp/src/Components/ExpenseCard/Expensecard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactSVG from 'react-svg';
+import foodIcon from '../../images/food.svg'
+
+const Expensecard = ({category, amount, date}) => (
+  <div className="card mb-4 shadow">
+    <div className="card-header d-flex align-items-center justify-content-between">
+      <img className="icons" src={`http://borbandibarkacs.hu/wp-content/uploads/2019/hackaton/${category}.svg`}/> 
+      <h3 className="text-center ml-5">{category}</h3>
+    </div>
+    <div className="d-flex">
+      <div className="card-body">
+        <h5 className="card-title d-inline">Amount: </h5><h5 className="d-inline text-danger">- {amount} Ft</h5>
+        <p className="card-text text-right"><small>{date}</small></p>
+      </div>
+    </div>
+  </div>
+);
+
+export default Expensecard;
--- 01-breakdown-webapp/src/Containers/HomePage/Homepage.js
+++ 01-breakdown-webapp/src/Containers/HomePage/Homepage.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Loading from '../../Components/Loading/Loading';
 import { connect } from 'react-redux';
-import { getAllUserInfo } from '../../actions/actions'
+import { getAllUserInfo } from '../../actions/actions';
+import Expensecard from '../../Components/ExpenseCard/Expensecard';
+
 class Homepage extends Component {
 
   componentDidMount = () => {
     this.props.getAllUserInfo("5c9ca6490650e135a4634b47");
-    console.log(this.dateMonth())
+    console.log(this.sevenDaysBefore());
   }
 
   handleClick = () => {
@@ -27,53 +29,75 @@ class Homepage extends Component {
     return monthNames[monthIndex] + ' ' + year;
   }
 
+  sevenDaysBefore = () => {
+    const d = Date.now();
+    const sevenDays = d - (7 * 24 * 60 * 60 * 1000);
+    console.log(sevenDays)
+    return sevenDays;
+  }
+
+  returnTimefromDate = date => (new Date(date).toLocaleDateString());
+
   render() {
     return (
-      <div onClick={this.handleClick}>
-        {!this.props.userIncome ? <Loading /> :
-          <div>
-            <div className="row">
-              <div className="col">
-                <div className="card">
-                  <div class="card-header">
-                    <h1 className="text-center">Summary</h1>
-                  </div>
-                  <div className="d-flex">
-                    <div className="card-body">
-                      <h5 className="card-title">{this.dateMonth()}</h5>
-                      <hr />
-                      <h6>Incomes: </h6><h6 className="text-success d-inline">{this.props.userSumIncome} Ft</h6>
-                      <h6>Expenses: </h6><h6 className="text-danger d-inline">{this.props.userSumExpense} Ft</h6>
-                      <h6>Summary: </h6><h6 className="font-weight-bold d-inline">{this.props.userSumIncome - this.props.userSumExpense} Ft</h6>
-                    </div>
+      !this.props.userIncome ? <Loading /> :
+        <div className="container" onClick={this.sevenDaysBefore}>
+          <div className="row w-100">
+            <div className="col">
+              <div className="card">
+                <div className="card-header">
+                  <h1 className="text-center">Summary</h1>
+                </div>
+                <div className="d-flex">
+                  <div className="card-body">
+                    <h5 className="card-title">{this.dateMonth()}</h5>
+                    <hr />
+                    <h6>Incomes: </h6><h6 className="text-success d-inline">{this.props.userSumIncome} Ft</h6>
+                    <h6>Expenses: </h6><h6 className="text-danger d-inline">{this.props.userSumExpense} Ft</h6>
+                    <h6>Summary: </h6><h6 className="font-weight-bold d-inline">{this.props.userSumIncome - this.props.userSumExpense} Ft</h6>
                   </div>
                 </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <div className="card">
-                  <div class="card-header">
-                    <h1 className="text-center">Expenses last 7 days</h1>
-                  </div>
-                  <div className="d-flex">
-                    <div className="card-body">
-                      <h5 className="card-title">{this.dateMonth()}</h5>
-                      <hr />
-                      <h6>Incomes: <h6 className="text-success d-inline">{this.props.userSumIncome} Ft</h6></h6>
-                      <h6>Expenses: <h6 className="text-danger d-inline">{this.props.userSumExpense} Ft</h6></h6>
-                      <h6>Summary: <h6 className="font-weight-bold d-inline">{this.props.userSumIncome - this.props.userSumExpense} Ft</h6></h6>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-6">
-
               </div>
             </div>
           </div>
-        }
-      </div>
+          <div className="row pt-3 w-100">
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  <h2 className="text-center">Expenses last 7 days</h2>
+                </div>
+                <div className="d-flex">
+                  <div className="card-body">
+                    {
+                      this.props.userExpense
+                        .filter(item => item.date > this.sevenDaysBefore())
+                        .map(element => {
+                          return (
+                            <Expensecard
+                              category={element.category}
+                              amount={element.amount}
+                              date={this.returnTimefromDate(element.date)}
+                            />
+                          )
+                        })
+                    }
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-6">
+              <div className="card">
+                <div className="card-header">
+                  <h2 className="text-center">Budgets</h2>
+                </div>
+                <div className="d-flex">
+                  <div className="card-body">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     )
   }
 }
