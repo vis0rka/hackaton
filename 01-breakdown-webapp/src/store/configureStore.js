eb870d79fbfd09e331f8e57f4036d3f01f00f26a
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-29 00:34:42 +0100

    fix(configureStore): createBrowserHistory fix

--- 01-breakdown-webapp/src/store/configureStore.js
+++ 01-breakdown-webapp/src/store/configureStore.js
@@ -1,23 +1,21 @@
-import rootReducer from '../reducers/index'
-import createHistory from 'history/createBrowserHistory';
-import { createStore, compose, applyMiddleware } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
-import createSagaMiddleware from 'redux-saga';
-import sayHelloSaga from '../sagas/sagas'
+import rootReducer from "../reducers/index";
+import { createBrowserHistory } from "history";
+import { createStore, compose, applyMiddleware } from "redux";
+import { routerMiddleware } from "react-router-redux";
+import createSagaMiddleware from "redux-saga";
+import sayHelloSaga from "../sagas/sagas";
 
-export const history = createHistory();
+export const history = createBrowserHistory();
 
 const reactRouterMiddleware = routerMiddleware(history);
 const sagaMiddleware = createSagaMiddleware();
 
-const middlewares = [
-  sagaMiddleware,
-  reactRouterMiddleware,
-];
+const middlewares = [sagaMiddleware, reactRouterMiddleware];
 
-const Store = createStore(rootReducer, compose(
-  applyMiddleware(...middlewares),
-));
+const Store = createStore(
+  rootReducer,
+  compose(applyMiddleware(...middlewares))
+);
 
 sagaMiddleware.run(sayHelloSaga);
 
