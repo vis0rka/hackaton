8dcc152504bb90520f91f469ce107c2513315a83
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-29 01:49:17 +0100

    feat(actions.js): add actions for the forms

--- 01-breakdown-webapp/src/actions/actions.js
+++ 01-breakdown-webapp/src/actions/actions.js
@@ -1,30 +1,50 @@
 export const sendUserLogin = (username, password) => ({
-  type: 'USER_LOGIN_REQUESTED',
+  type: "USER_LOGIN_REQUESTED",
   payload: {
     username,
-    password,
-  },
-})
+    password
+  }
+});
 
 export const sendUserRegister = (username, password) => ({
-  type: 'USER_REG_REQUESTED', 
+  type: "USER_REG_REQUESTED",
   payload: {
-    username, 
-    password,
-  },
-})
+    username,
+    password
+  }
+});
 
 export const clearErrorMessage = () => ({
-  type: 'USER_ERROR_MESSAGE_CLEAR'
-})
+  type: "USER_ERROR_MESSAGE_CLEAR"
+});
 
 export const clearLoginStatus = () => ({
-  type: 'USER_LOGIN_STATUS_CLEAR'
-})
+  type: "USER_LOGIN_STATUS_CLEAR"
+});
 
-export const getAllUserInfo = (userId) => ({
-  type: 'GET_ALL_USER_INFO_REQUESTED',
+export const getAllUserInfo = userId => ({
+  type: "GET_ALL_USER_INFO_REQUESTED",
   payload: {
     userId
   }
-})
+});
+
+export const sendDebit = data => ({
+  type: "SEND_DEBIT",
+  data
+});
+
+export const sendIncome = data => ({
+  type: "SEND_INCOME",
+  data
+});
+
+export const sendExpense = data => ({
+  type: "SEND_EXPENSE",
+  data
+});
+
+export const sendBudget = data => ({
+  type: "SEND_BUDGET",
+  data
+});
