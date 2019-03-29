11d1740e3fe763d05c00ca5932c0875cc677e5e3
Laszlo Juhasz <ifj.juhasz.laszlo@gmail.com>
2019-03-29 01:46:58 +0100

    feat(backend - income): add description field

--- 00-breakdown-server/models/income.js
+++ 00-breakdown-server/models/income.js
@@ -1,17 +1,21 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
 const Income = new Schema({
   amount: {
     type: Number,
-    required: true,
+    required: true
+  },
+  description: {
+    type: String,
+    default: "Other"
   },
   date: {
     type: Number,
     timestamp: true,
-    default: Date.now,
+    default: Date.now
   }
-})
+});
 
-module.exports = mongoose.model('Income', Income);
+module.exports = mongoose.model("Income", Income);
